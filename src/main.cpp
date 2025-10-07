9e0d18a0341cb0b197efed47e1f451ba60d3936e
Jadoooooou <2366906@cmontmorency.qc.ca>
2025-10-07 16:40:37 -0400

    pixel_key_jaune_HUB

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,42 @@
 #include <MicroOscSlip.h>
 MicroOscSlip<128> monOsc(&Serial);
 
-// key unit
+// gestion du hub
+#include <M5_PbHub.h>
+M5_PbHub myPbHub;
+
 #define BROCHE_ATOM_FIL_BLANC 32
-// led in key unit
 #define BROCHE_ATOM_FIL_JAUNE 26
-// bouton du Atom
 #define BROCHE_ATOM_BOUTON 39
-// led du Atom
 #define BROCHE_ATOM_PIXEL 27
 
-// pixel du key unit
-CRGB keyPixel;
-// pixel du Atom
+// channel du hub où il est utilisé
+#define KEY_CHANNEL 0
+
+//CRGB keyPixel;
 CRGB atomPixel;
 
 
 void setup() {
-  pinMode( BROCHE_ATOM_FIL_BLANC , INPUT_PULLUP );
+  //pinMode( BROCHE_ATOM_FIL_BLANC , INPUT_PULLUP );
   pinMode( BROCHE_ATOM_BOUTON , INPUT );
 
-  FastLED.addLeds< WS2812, BROCHE_ATOM_FIL_JAUNE , GRB >(&keyPixel, 1); 
+  //FastLED.addLeds< WS2812, BROCHE_ATOM_FIL_JAUNE , GRB >(&keyPixel, 1); 
   FastLED.addLeds<WS2812, BROCHE_ATOM_PIXEL , GRB>(&atomPixel, 1); 
+
+  Wire.begin();
+  myPbHub.begin();
+
+  myPbHub.setPixelCount( KEY_CHANNEL ,  1 );
+
 }
 
 void loop() {
 
   // oscslip key unit
-  int maLectureKey = digitalRead( BROCHE_ATOM_FIL_BLANC );
+  //int maLectureKey = digitalRead( BROCHE_ATOM_FIL_BLANC );
+  int maLectureKey = myPbHub.digitalRead(KEY_CHANNEL);
+
   // oscslip bouton du Atom
   int maLectureAtomBouton = digitalRead( BROCHE_ATOM_BOUTON );
 
@@ -37,9 +46,11 @@ void loop() {
 
   // key unit
   if (maLectureKey == 0) {
-    keyPixel = CRGB(255, 255, 0);
+    //keyPixel = CRGB(255, 255, 0);
+    myPbHub.setPixelColor(KEY_CHANNEL, 0, 127, 250, 0);
   } else {
-     keyPixel = CRGB(0, 0, 0);
+    //keyPixel = CRGB(0, 0, 0);
+    myPbHub.setPixelColor(KEY_CHANNEL, 0, 0, 0, 0);
   }
 
   // bouton du Atom
