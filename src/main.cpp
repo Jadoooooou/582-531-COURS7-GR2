02787afbcc2f150c409f2058b2a6e3de45a58bbf
Jadoooooou <2366906@cmontmorency.qc.ca>
2025-10-07 15:57:12 -0400

    tous_btn_et_lumieres_integres

--- src/main.cpp
+++ src/main.cpp
@@ -7,34 +7,47 @@ MicroOscSlip<128> monOsc(&Serial);
 #define BROCHE_ATOM_FIL_BLANC 32
 // led in key unit
 #define BROCHE_ATOM_FIL_JAUNE 26
+// bouton du Atom
+#define BROCHE_ATOM_BOUTON 39
+// led du Atom
+#define BROCHE_ATOM_PIXEL 27
 
+// pixel du key unit
 CRGB keyPixel;
+// pixel du Atom
+CRGB atomPixel;
+
 
 void setup() {
   pinMode( BROCHE_ATOM_FIL_BLANC , INPUT_PULLUP );
-  Serial.begin(115200);
+  pinMode( BROCHE_ATOM_BOUTON , INPUT );
+
   FastLED.addLeds< WS2812, BROCHE_ATOM_FIL_JAUNE , GRB >(&keyPixel, 1); 
+  FastLED.addLeds<WS2812, BROCHE_ATOM_PIXEL , GRB>(&atomPixel, 1); 
 }
 
 void loop() {
 
-  // oscslip_bouton
+  // oscslip key unit
   int maLectureKey = digitalRead( BROCHE_ATOM_FIL_BLANC );
+  // oscslip bouton du Atom
+  int maLectureAtomBouton = digitalRead( BROCHE_ATOM_BOUTON );
 
-  // 0 et 1 pour appuyer et relacher
-  Serial.print(maLectureKey);
-  Serial.println();
   delay(100);
 
-  // envoit OSC
-  //monOsc.sendInt("/bouton", maLectureKey);
-  //delay(100);
-
+  // key unit
   if (maLectureKey == 0) {
     keyPixel = CRGB(255, 255, 0);
-     FastLED.show();
   } else {
      keyPixel = CRGB(0, 0, 0);
-     FastLED.show();
   }
+
+  // bouton du Atom
+  if (maLectureAtomBouton == 0) {
+    atomPixel = CRGB(255, 255, 0);
+  } else {
+    atomPixel = CRGB(0, 0, 0);
+  }
+
+  FastLED.show();
 }
