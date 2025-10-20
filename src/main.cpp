daf27f12743ca22420287c8d454694701b5975a8
Jadoooooou <2366906@cmontmorency.qc.ca>
2025-10-20 14:21:47 -0400

    devoir_01

--- src/main.cpp
+++ src/main.cpp
@@ -20,45 +20,22 @@ CRGB atomPixel;
 
 
 void setup() {
-  //pinMode( BROCHE_ATOM_FIL_BLANC , INPUT_PULLUP );
-  pinMode( BROCHE_ATOM_BOUTON , INPUT );
 
-  //FastLED.addLeds< WS2812, BROCHE_ATOM_FIL_JAUNE , GRB >(&keyPixel, 1); 
-  FastLED.addLeds<WS2812, BROCHE_ATOM_PIXEL , GRB>(&atomPixel, 1); 
+  // NE JAMAIS OUBLIER !!!
+  Serial.begin(115200);
+
+  pinMode( BROCHE_ATOM_FIL_BLANC , INPUT_PULLUP );
 
   Wire.begin();
   myPbHub.begin();
-
-  myPbHub.setPixelCount( KEY_CHANNEL ,  1 );
 
 }
 
 void loop() {
 
   // oscslip key unit
-  //int maLectureKey = digitalRead( BROCHE_ATOM_FIL_BLANC );
-  int maLectureKey = myPbHub.digitalRead(KEY_CHANNEL);
-
-  // oscslip bouton du Atom
-  int maLectureAtomBouton = digitalRead( BROCHE_ATOM_BOUTON );
-
+  int press = myPbHub.digitalRead(KEY_CHANNEL);
+  monOsc.sendInt("/decollage", press);
   delay(100);
-
-  // key unit
-  if (maLectureKey == 0) {
-    //keyPixel = CRGB(255, 255, 0);
-    myPbHub.setPixelColor(KEY_CHANNEL, 0, 127, 250, 0);
-  } else {
-    //keyPixel = CRGB(0, 0, 0);
-    myPbHub.setPixelColor(KEY_CHANNEL, 0, 0, 0, 0);
-  }
-
-  // bouton du Atom
-  if (maLectureAtomBouton == 0) {
-    atomPixel = CRGB(255, 255, 0);
-  } else {
-    atomPixel = CRGB(0, 0, 0);
-  }
-
-  FastLED.show();
+  
 }
